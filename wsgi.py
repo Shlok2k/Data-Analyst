ab04036f1aefd76ba216e3e9411b512edcd89233
SHLOKE SONKER <150910144+Shlok2k@users.noreply.github.com>
2025-05-27 11:30:42 +0530

    Add files via upload

---
+++ wsgi.py
@@ -0,0 +1,14 @@
+from app import app
+
+# This is for Gunicorn
+application = app.server
+
+# This is for local development
+if __name__ == "__main__":
+    from waitress import serve
+    print("Starting production server...")
+    print("Server will be available at:")
+    print("http://127.0.0.1:8050")
+    print("http://192.168.29.243:8050")
+    print("Press CTRL+C to stop the server")
+    serve(application, host='0.0.0.0', port=8050, threads=4) 
